4d634a43954181170aab95b01470bb613ec734bd
sean <sean@pavilion>
2019-04-25 21:04:23 +0100

    add a functional test

--- apps/php-test/tests/Controller/LuckyControllerTest.php
+++ apps/php-test/tests/Controller/LuckyControllerTest.php
@@ -1,10 +1,24 @@
 <?php
 
-
 namespace Test\App\Controller;
 
+use App\Controller\LuckyController;
+use PHPUnit\Framework\Assert;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class LuckyControllerTest
+class LuckyControllerTest extends WebTestCase
 {
+    /**
+     * @see https://symfony.com/doc/current/testing.html#your-first-functional-test
+     */
+    public function testNumber()
+    {
+        $client = static::createClient();
 
+        $crawler = $client->request('GET', '/lucky/number');
+
+        Assert::assertEquals(200, $client->getResponse()->getStatusCode());
+
+        Assert::assertGreaterThan(0, $crawler->filter('h1:contains("Your lucky number is")')->count());
+    }
 }
